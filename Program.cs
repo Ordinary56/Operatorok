6bb5a826aafaf48d0c97a9aed41e66be120536d2
tbalogh695@gmail.com <tbalogh695@gmail.com>
2023-05-24 21:56:45 +0200

    ROSSZ KÓD LETT ÁTMÁSOLVA!!

--- Program.cs
+++ Program.cs
@@ -1,90 +1,75 @@
-﻿//usng System;
-//using System.Collection.Generics;
-//using System.Linq;
-namespace LINQ_assignment
+﻿List<string> kifejezesek = new List<string>();
+using (StreamReader sr = new("./kifejezesek.txt"))
 {
-    public class Program
+    while (!sr.EndOfStream)
     {
-        static void Main(string[] args)
-        {
-            List<int> napiCsapadek = new();
-            List<string> versenyzok = new()
-            {
-                "Kovács-Béla",
-                "Gipsz-Jakab",
-                "Papp-András",
-                "Varga-Katalin",
-                "Szabó-Péter",
-                "Kiss-Ádám",
-                "Nagy-Dávid"
-            };
-            List<double> maxHofok = new();
-            List<string> diakok = new(){
-                "Gipsz Jakab",
-                "Lakatos Krisztina Mária",
-                "Benedek Elek",
-                "Kiss Sándor",
-                "Veréb András János"
-            };
+        kifejezesek.Add(sr.ReadLine()!);
+    }
+}
 
-            //napiCsapadék feltöltése randopm adatokkal
-            Enumerable.Range(1, 365).ToList().ForEach(x => napiCsapadek.Add(new Random().Next(1, 301)));
-            //maxHofok feltöltése randopm adatokkal
-            Enumerable.Range(1, 30).ToList().ForEach(x => maxHofok.Add(new Random().NextDouble() * 30));
+//2. feladat 
+Console.WriteLine($"2. feladat: Kifejezések száma: {kifejezesek.Count()}");
 
+//3. feladat
+Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {kifejezesek.Count(x => x.Split(' ')[1].Equals("mod"))}");
 
-            //1. feladat
-            Console.WriteLine(maxHofok.Count(x => x > 20));
-            //2. feladat
-            List<double> hofokok = maxHofok.OrderByDescending(x => x).ToList();
-            //3. feladat
-            Console.WriteLine($"Ennyi tanulónak van 2 keresztneve: {diakok.Count(x => x.Split(' ').Length == 3)}");
-            //4. feladat
-            foreach (string diak in diakok.OrderBy(x => x))
-            {
-                if (diak.Trim().Length > 15) Console.WriteLine(diak);
-            }
-            //5. feladat
-            napiCsapadek.Count(x => x > 10);
-            //6.feladat
-            napiCsapadek.OrderByDescending(x => x);
-            //7.feladat
-            List<int> aligesett = napiCsapadek.Where(x => x < 3).ToList();
-            //8. feladat
-            versenyzok.OrderBy(x => x.Trim().Length).ThenBy(x => x);
-            //9.feladat
-            napiCsapadek.Count(x => x.Equals(0));
-            //10. feladat
-            List<int> sokeso = napiCsapadek.Where(x => x > 5).ToList();
-            //11. feladat
-            versenyzok
-            .Where(x => x.Split('-')[1].Equals("Dávid"))
-            .ToList().ForEach(x => Console.WriteLine(x));
-            //12. feladat
-            Console.WriteLine($"A legnagyobb hőmérséklet különbség: {maxHofok.Max() - maxHofok.Min()}");
-            //13. feladat
-            Console.WriteLine(napiCsapadek.OrderBy(x => x).ToList()[1]);
-            //14. feladat   
-            System.Console.WriteLine(napiCsapadek.Average());
-            //15. feladat
-            System.Console.WriteLine(versenyzok.Any(x => x.Equals("Szuper-Béla")) ? "Van ilyen versenyző"
-            : "Nincs ilyen versenyző");
-            //16. feladat
-            napiCsapadek.FindLastIndex(x => x > 30);
-            //17. feladat
-            diakok.DistinctBy(x => x.Split(' ')[0]).Count();
-            //18. feladat
-            versenyzok.Find(x => x.Contains("Vajk"));
-            //19. feladat
-            List<string> nevek = diakok.Select(x => x.Split(' ')[1]).ToList();
-            //20. feladat
-            List<string> angolul = versenyzok
-            .Where(nev => nev.Contains("Béla"))
-            .Select(x => x.Split('-')[1] + " " + x.Split('-')[0])
-            .ToList();
+//4.feladat
+Console.WriteLine("4. feladat: {0}", kifejezesek.Any(x => int.Parse(x.Split(' ')[^1]) != 0 && int.Parse(x.Split(' ')[0]) % int.Parse(x.Split(' ')[^1]) == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés!");
 
-        }
+//5. feladat
+Console.WriteLine($"5. feladat: Statisztika:" +
+    $"\n\tmod -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("mod"))} db" +
+    $"\n\t/ -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("/"))} db" +
+    $"\n\tdiv -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("div"))} db" +
+    $"\n\t- -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("-"))} db" +
+    $"\n\t* -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("*"))} db" +
+    $"\n\t+ -> {kifejezesek.Count(x => x.Split(' ')[1].Equals("+"))} db");
 
+//7. feladat
+string result = string.Empty;
+while (!result.Equals("vége"))
+{
+    Console.Write("7.feladat: Kérek egy kifejezést (pl.:1+1):");
+    result = Console.ReadLine()!;
+    Console.WriteLine(Szamolas(result));
+}
+
+//8. feladat
+using (StreamWriter sw = new("./eredmények.txt"))
+{
+    kifejezesek.ForEach(x => sw.WriteLine(Szamolas(x)));
+}
+
+//6. feladat
+static string Szamolas(string expression)
+{
+    string result;
+    if (string.IsNullOrEmpty(expression) || expression.Equals("vége"))
+    {
+        return string.Empty;
+    }
+    Dictionary<string, Func<double, double, double>> operatorDictionary = new Dictionary<string, Func<double, double, double>>()
+    {
+        { "+", (a,b) => a+ b},
+        { "-", (a,b) => a-b},
+        { "*", (a,b) => a*b},
+        { "/", (a,b) => a/b},
+        { "mod", (a,b) => a%b},
+        { "div", (a,b) => Math.Floor(a / b)},
+    };
+    try
+    {
+        result = $"{operatorDictionary[expression.Split(' ')[1]]
+        (double.Parse(expression.Split(' ')[0]), double.Parse(expression.Split(' ')[^1]))}";
+    }
+    catch (KeyNotFoundException)
+    {
+        result = "Hibás operátor!";
     }
 
+    catch (Exception)
+    {
+        result = "Egyéb hiba!";
+    }
+    return expression + $" = {result}";
 }
